2d248968b3bd4b871f04a4c19e1258d846d401e8
Oleksandr <oleksandr.ivanchuk@volvocars.com>
2019-03-01 15:02:38 +0200

    Add compiled binary to flash the board

---
+++ build-STM32F4NUCLEO/genhdr/pins.h
@@ -0,0 +1,137 @@
+extern const pin_obj_t pin_A0;
+extern const pin_af_obj_t pin_A0_af[];
+extern const pin_obj_t pin_A1;
+extern const pin_af_obj_t pin_A1_af[];
+extern const pin_obj_t pin_A2;
+extern const pin_af_obj_t pin_A2_af[];
+extern const pin_obj_t pin_A3;
+extern const pin_af_obj_t pin_A3_af[];
+extern const pin_obj_t pin_A4;
+extern const pin_af_obj_t pin_A4_af[];
+extern const pin_obj_t pin_A5;
+extern const pin_af_obj_t pin_A5_af[];
+extern const pin_obj_t pin_A6;
+extern const pin_af_obj_t pin_A6_af[];
+extern const pin_obj_t pin_A7;
+extern const pin_af_obj_t pin_A7_af[];
+extern const pin_obj_t pin_A8;
+extern const pin_af_obj_t pin_A8_af[];
+extern const pin_obj_t pin_A9;
+extern const pin_af_obj_t pin_A9_af[];
+extern const pin_obj_t pin_A10;
+extern const pin_af_obj_t pin_A10_af[];
+extern const pin_obj_t pin_A13;
+extern const pin_obj_t pin_A14;
+extern const pin_obj_t pin_A15;
+extern const pin_af_obj_t pin_A15_af[];
+extern const pin_obj_t pin_B0;
+extern const pin_af_obj_t pin_B0_af[];
+extern const pin_obj_t pin_B1;
+extern const pin_af_obj_t pin_B1_af[];
+extern const pin_obj_t pin_B2;
+extern const pin_obj_t pin_B4;
+extern const pin_af_obj_t pin_B4_af[];
+extern const pin_obj_t pin_B5;
+extern const pin_af_obj_t pin_B5_af[];
+extern const pin_obj_t pin_B6;
+extern const pin_af_obj_t pin_B6_af[];
+extern const pin_obj_t pin_B7;
+extern const pin_af_obj_t pin_B7_af[];
+extern const pin_obj_t pin_B8;
+extern const pin_af_obj_t pin_B8_af[];
+extern const pin_obj_t pin_B9;
+extern const pin_af_obj_t pin_B9_af[];
+extern const pin_obj_t pin_B10;
+extern const pin_af_obj_t pin_B10_af[];
+extern const pin_obj_t pin_B12;
+extern const pin_af_obj_t pin_B12_af[];
+extern const pin_obj_t pin_B13;
+extern const pin_af_obj_t pin_B13_af[];
+extern const pin_obj_t pin_B14;
+extern const pin_af_obj_t pin_B14_af[];
+extern const pin_obj_t pin_B15;
+extern const pin_af_obj_t pin_B15_af[];
+extern const pin_obj_t pin_C0;
+extern const pin_obj_t pin_C1;
+extern const pin_obj_t pin_C2;
+extern const pin_af_obj_t pin_C2_af[];
+extern const pin_obj_t pin_C3;
+extern const pin_af_obj_t pin_C3_af[];
+extern const pin_obj_t pin_C4;
+extern const pin_obj_t pin_C5;
+extern const pin_obj_t pin_C6;
+extern const pin_af_obj_t pin_C6_af[];
+extern const pin_obj_t pin_C7;
+extern const pin_af_obj_t pin_C7_af[];
+extern const pin_obj_t pin_C8;
+extern const pin_af_obj_t pin_C8_af[];
+extern const pin_obj_t pin_C9;
+extern const pin_af_obj_t pin_C9_af[];
+extern const pin_obj_t pin_C10;
+extern const pin_af_obj_t pin_C10_af[];
+extern const pin_obj_t pin_C11;
+extern const pin_af_obj_t pin_C11_af[];
+extern const pin_obj_t pin_C12;
+extern const pin_af_obj_t pin_C12_af[];
+extern const pin_obj_t pin_C13;
+extern const pin_obj_t pin_C14;
+extern const pin_obj_t pin_C15;
+extern const pin_obj_t pin_D2;
+extern const pin_af_obj_t pin_D2_af[];
+extern const pin_obj_t pin_H0;
+extern const pin_obj_t pin_H1;
+extern const pin_obj_t * const pin_adc1[];
+extern const pin_obj_t * const pin_adc2[];
+extern const pin_obj_t * const pin_adc3[];
+#define pyb_pin_PC0 pin_C0
+#define pyb_pin_PC1 pin_C1
+#define pyb_pin_PC2 pin_C2
+#define pyb_pin_PC3 pin_C3
+#define pyb_pin_PA0 pin_A0
+#define pyb_pin_PA1 pin_A1
+#define pyb_pin_PA2 pin_A2
+#define pyb_pin_PA3 pin_A3
+#define pyb_pin_PA4 pin_A4
+#define pyb_pin_PA5 pin_A5
+#define pyb_pin_PA6 pin_A6
+#define pyb_pin_PA7 pin_A7
+#define pyb_pin_PC4 pin_C4
+#define pyb_pin_PC5 pin_C5
+#define pyb_pin_PB0 pin_B0
+#define pyb_pin_PB1 pin_B1
+#define pyb_pin_PB2 pin_B2
+#define pyb_pin_PB10 pin_B10
+#define pyb_pin_PB12 pin_B12
+#define pyb_pin_PB13 pin_B13
+#define pyb_pin_PB14 pin_B14
+#define pyb_pin_PB15 pin_B15
+#define pyb_pin_PC6 pin_C6
+#define pyb_pin_PC7 pin_C7
+#define pyb_pin_PC8 pin_C8
+#define pyb_pin_PC9 pin_C9
+#define pyb_pin_PA8 pin_A8
+#define pyb_pin_PA9 pin_A9
+#define pyb_pin_PA10 pin_A10
+#define pyb_pin_PA13 pin_A13
+#define pyb_pin_PA14 pin_A14
+#define pyb_pin_PA15 pin_A15
+#define pyb_pin_PC10 pin_C10
+#define pyb_pin_PC11 pin_C11
+#define pyb_pin_PC12 pin_C12
+#define pyb_pin_PD2 pin_D2
+#define pyb_pin_PB4 pin_B4
+#define pyb_pin_PB5 pin_B5
+#define pyb_pin_PB6 pin_B6
+#define pyb_pin_PB7 pin_B7
+#define pyb_pin_PB8 pin_B8
+#define pyb_pin_PB9 pin_B9
+#define pyb_pin_PC13 pin_C13
+#define pyb_pin_PC14 pin_C14
+#define pyb_pin_PC15 pin_C15
+#define pyb_pin_PH0 pin_H0
+#define pyb_pin_PH1 pin_H1
+#define pyb_pin_LED_GREEN pin_A5
+#define pyb_pin_LED_ORANGE pin_A5
+#define pyb_pin_LED_RED pin_A5
+#define pyb_pin_LED_BLUE pin_A4
+#define pyb_pin_SW pin_C13
